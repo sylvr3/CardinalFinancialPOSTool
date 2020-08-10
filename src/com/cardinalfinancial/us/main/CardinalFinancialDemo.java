c53b65555d12668313e70b43e35ae5a6278c1565
IFS Developer <ifsdeveloper@MacBook-Pro-4.domain>
2020-08-09 22:36:50 -0700

    Refactored calculateChargeableDaysCount method and broke up functionality into multiple methods to clean up and reduce complexity of code, modified files for gradle script

---
+++ src/com/cardinalfinancial/us/main/CardinalFinancialDemo.java
@@ -0,0 +1,111 @@
+package com.cardinalfinancial.us.main;
+
+import com.cardinalfinancial.us.model.Checkout;
+import com.cardinalfinancial.us.model.RentalAgreement;
+import com.cardinalfinancial.us.model.Tool;
+import com.cardinalfinancial.us.service.CheckoutService;
+import com.cardinalfinancial.us.service.CheckoutServiceImpl;
+
+public class CardinalFinancialDemo {
+
+    public static void main(String[] args) {
+        CheckoutService checkoutService = new CheckoutServiceImpl();
+
+        // Test 1:
+
+        /*System.out.println("***** Test 1 *****");
+         Exception in thread "main" java.lang.IllegalArgumentException: Discount percent must be between 0 and 100. Please enter the discount percent for the tool rental.
+	at com.cardinalfinancial.us.model.Checkout$Builder.setDiscountPercent(Checkout.java:134)
+	at com.cardinalfinancial.us.test.CardinalFinancialDemo.main(CardinalFinancialDemo.java:28)
+        Tool jackhammer = Tool.JAKR;
+
+        Checkout checkout = Checkout.Builder.newInstance()
+                .setToolCode(jackhammer.getToolCode().getValue())
+                .setRentalDayCount(5)
+                .setDiscountPercent(101)
+                .setCheckoutDate("9/3/15")
+                .build();
+
+        checkoutService.processCheckout(checkout);*/
+
+
+        // Test 2:
+        System.out.println("***** Test 2 *****");
+        Tool ladder = Tool.LADW;
+        Checkout checkout2 = Checkout.Builder.newInstance()
+                .setToolCode(ladder.getToolCode().getValue())
+                .setRentalDayCount(3)
+                .setDiscountPercent(10)
+                .setCheckoutDate("7/2/20")
+                .build();
+
+        RentalAgreement rentalAgreement2 = checkoutService.processCheckout(checkout2);
+
+        rentalAgreement2.printRentalAgreementDetails();
+
+
+        // Test 3
+        System.out.println("***** Test 3 *****");
+        Tool chainsaw = Tool.CHNS;
+
+        Checkout checkout3 = Checkout.Builder.newInstance()
+                .setToolCode(chainsaw.getToolCode().getValue())
+                .setRentalDayCount(5)
+                .setDiscountPercent(25)
+                .setCheckoutDate("7/2/15")
+                .build();
+
+        RentalAgreement rentalAgreement3 = checkoutService.processCheckout(checkout3);
+        rentalAgreement3.printRentalAgreementDetails();
+
+
+        // Test 4
+        System.out.println("***** Test 4 *****");
+        Tool jackhammer2 = Tool.JAKD;
+
+        Checkout checkout4 = Checkout.Builder.newInstance()
+                .setToolCode(jackhammer2.getToolCode().getValue())
+                .setRentalDayCount(6)
+                .setDiscountPercent(0)
+                .setCheckoutDate("9/3/15")
+                .build();
+
+        RentalAgreement rentalAgreement4 = checkoutService.processCheckout(checkout4);
+        rentalAgreement4.printRentalAgreementDetails();
+
+
+        // Test 5
+        System.out.println("***** Test 5 *****");
+
+        Tool jackhammer3 = Tool.JAKR;
+        Checkout checkout5 = Checkout.Builder.newInstance()
+                .setToolCode(jackhammer3.getToolCode().getValue())
+                .setRentalDayCount(9)
+                .setDiscountPercent(0)
+                .setCheckoutDate("7/2/15")
+                .build();
+
+        RentalAgreement rentalAgreement5 = checkoutService.processCheckout(checkout5);
+        rentalAgreement5.printRentalAgreementDetails();
+
+
+        // Test 6
+
+        System.out.println("***** Test 6 *****");
+
+        Tool jackhammer4 = Tool.JAKR;
+
+        Checkout checkout6 = Checkout.Builder.newInstance()
+                .setToolCode(jackhammer4.getToolCode().getValue())
+                .setRentalDayCount(4)
+                .setDiscountPercent(50)
+                .setCheckoutDate("7/2/20") // input date format can be M/d/yy or MM/dd/yy
+                .build();
+
+        RentalAgreement rentalAgreement6 = checkoutService.processCheckout(checkout6);
+        rentalAgreement6.printRentalAgreementDetails();
+
+
+    }
+
+}
--- src/com/cardinalfinancial/us/utils/CalculateChargesUtils.java
+++ src/com/cardinalfinancial/us/utils/CalculateChargesUtils.java
@@ -29,8 +29,16 @@ public class CalculateChargesUtils {
     public static final String CHARGE_HOLIDAYS = "CHARGE_HOLIDAYS";
     public static final String CHARGE_BOTH = "CHARGE_BOTH";
 
-    public static int calculateChargeableDaysCount(Tool tool, Checkout checkout) {
 
+    /**
+     * Determine how many days should be charged based on whether holidays or weekend charges apply for a particular tool
+     *
+     * @param tool     Tool instance
+     * @param checkout Checkout instance
+     * @return the number of chargeable days
+     **/
+
+    public static int calculateChargeableDaysCount(Tool tool, Checkout checkout) {
         // number of chargeable days
         int chargeableDaysCount = 0;
 
@@ -39,54 +47,10 @@ public class CalculateChargesUtils {
             LocalDate date = DateUtils.parseInputDate(checkout.getCheckoutDate()).plusDays(1);
             // Create due date instance from checkout date and rental day count
             LocalDate dueDate = DateUtils.parseInputDate(checkout.getCheckoutDate()).plusDays(checkout.getRentalDayCount());
-
+            String isHolidayChargeStr = tool.getToolType().isHolidayCharge();
+            String isWeekendChargeStr = tool.getToolType().isWeekendCharge();
             while (!date.isEqual(dueDate.plusDays(1))) {
-                // Tool has no holiday charge and weekend charge
-                if (tool.getToolType().isHolidayCharge().equals(ToolConstants.NO_CHARGE) && tool.getToolType().isWeekendCharge().equals(ToolConstants.NO_CHARGE)) {
-                    // check if the date is 4th of July
-                    if (tool.getToolType().isHolidayCharge().equals(ToolConstants.NO_CHARGE) && tool.getToolType().isWeekendCharge().equals(ToolConstants.NO_CHARGE) && isFourthOfJuly(date)
-                            && (date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY)) {
-                        // if 4th of July is on Saturday or Sunday, weekday needs to be subtracted from chargeableDaysCount
-                        chargeableDaysCount--;
-                    }
-
-                    // If it is Labor Day or a weekend day, then do not increment chargeableDaysCount
-                    else if (!isLaborDay(date)) {
-                        if (date.getDayOfWeek() != DayOfWeek.SATURDAY && date.getDayOfWeek() != DayOfWeek.SUNDAY) {
-                            chargeableDaysCount++;
-                        }
-                    }
-
-                }
-
-                // Tool has no weekend charge but it has holiday charge
-                else if (tool.getToolType().isWeekendCharge().equals(ToolConstants.NO_CHARGE) && tool.getToolType().isHolidayCharge().equals(ToolConstants.YES_CHARGE)) {
-                    // If it is the weekend, then do not increment the chargeableDaysCount. If it is a weekday, then increment chargeableDaysCount
-                    if (date.getDayOfWeek() != DayOfWeek.SATURDAY && date.getDayOfWeek() != DayOfWeek.SUNDAY) {
-                        chargeableDaysCount++;
-                    }
-
-                }
-
-                // Tool has no holiday charge, but has weekend charge
-                else if (tool.getToolType().isHolidayCharge().equals(ToolConstants.NO_CHARGE) && tool.getToolType().isWeekendCharge().equals(ToolConstants.YES_CHARGE)) {
-                    // check that the date is July 4th
-                    if (isFourthOfJuly(date)) {
-                        // Check if the date is Saturday or Sunday and if the due date is on Saturday......
-                        // if the due date is on Sunday, they do not get a free day for Sunday
-                        if (((date.getDayOfWeek() == DayOfWeek.SATURDAY) && (DateUtils.parseInputDate(checkout.getCheckoutDate()).isEqual(date.minusDays(1))))
-                                || ((date.getDayOfWeek() == DayOfWeek.SUNDAY) && (LocalDate.parse(DateUtils.calculateDueDate(checkout.getCheckoutDate(), checkout.getRentalDayCount())).isEqual(date)))) {
-                            chargeableDaysCount++;
-                        }
-                    }
-
-                    // If it is not Labor Day, increment chargeableDaysCount charged by default
-                    else if (!isLaborDay(date)) {
-                        chargeableDaysCount++;
-
-                    }
-
-                }
+                chargeableDaysCount = determineWhetherThereAreCharges(isHolidayChargeStr, isWeekendChargeStr, date, checkout, chargeableDaysCount);
                 // Increment the date by 1 day
                 date = date.plusDays(1);
             }
@@ -97,6 +61,63 @@ public class CalculateChargesUtils {
         }
     }
 
+
+    public static int determineWhetherThereAreCharges(String isThereHolidayCharge, String isThereWeekendCharge, LocalDate date, Checkout checkout, int numberOfChargeableDays) {
+        if (isThereHolidayCharge.equals(ToolConstants.NO_CHARGE) && isThereWeekendCharge.equals(ToolConstants.NO_CHARGE)) {
+            // check if the date is 4th of July
+            numberOfChargeableDays = isItFourthOfJulyAndWeekend(date, numberOfChargeableDays);
+        }
+
+        // Tool has no weekend charge but it has holiday charge
+        else if (isThereHolidayCharge.equals(ToolConstants.YES_CHARGE) && isThereWeekendCharge.equals(ToolConstants.NO_CHARGE)) {
+            // If it is the weekend, then do not increment the chargeableDaysCount. If it is a weekday, then increment chargeableDaysCount
+            numberOfChargeableDays = isItTheWeekend(date, numberOfChargeableDays);
+
+        }
+        if (isThereHolidayCharge.equals(ToolConstants.NO_CHARGE) && isThereWeekendCharge.equals(ToolConstants.YES_CHARGE)) {
+            if (isFourthOfJuly(date)) {
+                // Check if the date is Saturday or Sunday and if the due date is on Saturday......
+                // if the due date is on Sunday, they do not get a free day for Sunday
+                numberOfChargeableDays = incrementOrDecrementChargeableDaysCount(date, checkout, numberOfChargeableDays);
+            }
+
+            // If it is not Labor Day, increment chargeableDaysCount charged by default
+            else if (!isLaborDay(date)) {
+                numberOfChargeableDays++;
+
+            }
+        }
+
+        return numberOfChargeableDays;
+    }
+
+    public static int isItFourthOfJulyAndWeekend(LocalDate date, int numberOfChargeableDays) {
+        if (isFourthOfJuly(date)
+                && (date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY)) {
+            // if 4th of July is on Saturday or Sunday, weekday needs to be subtracted from chargeableDaysCount
+            numberOfChargeableDays--;
+        }
+        // If it is Labor Day or a weekend day, then do not increment chargeableDaysCount
+        else if (!isLaborDay(date)) {
+            numberOfChargeableDays = isItTheWeekend(date, numberOfChargeableDays);
+        }
+        return numberOfChargeableDays;
+    }
+
+    public static int isItTheWeekend(LocalDate date, int numberOfChargeableDays) {
+        if (date.getDayOfWeek() != DayOfWeek.SATURDAY && date.getDayOfWeek() != DayOfWeek.SUNDAY) {
+            numberOfChargeableDays++;
+        }
+        return numberOfChargeableDays;
+    }
+
+    public static int incrementOrDecrementChargeableDaysCount(LocalDate date, Checkout checkout, int numberOfChargeableDays) {
+        if (((date.getDayOfWeek() == DayOfWeek.SATURDAY) && (DateUtils.parseInputDate(checkout.getCheckoutDate()).isEqual(date.minusDays(1))))
+                || ((date.getDayOfWeek() == DayOfWeek.SUNDAY) && (LocalDate.parse(DateUtils.calculateDueDate(checkout.getCheckoutDate(), checkout.getRentalDayCount())).isEqual(date)))) {
+            numberOfChargeableDays++;
+        }
+        return numberOfChargeableDays;
+    }
 
     /**
      * Calculates the pre-discount charge as charge days X daily charge. Resulting total rounded to cents.
@@ -151,10 +172,7 @@ public class CalculateChargesUtils {
      * @return true if it is on 4th of July, false if it is not
      */
     public static boolean isFourthOfJuly(LocalDate date) {
-        if (date.getMonthValue() == 7 && date.getDayOfMonth() == 4) {
-            return true;
-        }
-        return false;
+        return date.getMonthValue() == 7 && date.getDayOfMonth() == 4;
     }
 
     /**
@@ -164,8 +182,6 @@ public class CalculateChargesUtils {
      * @return true if it is on Labor Day, false if it is not
      */
     public static boolean isLaborDay(LocalDate date) {
-        if (date.getMonthValue() == 9 && date.getDayOfWeek() == DayOfWeek.MONDAY && date.getDayOfMonth() < 8)
-            return true;
-        return false;
+        return date.getMonthValue() == 9 && date.getDayOfWeek() == DayOfWeek.MONDAY && date.getDayOfMonth() < 8;
     }
 }
